f0364fd6753bdaec778a90e4003d000b37e6c08d
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-11-12 17:27:43 -0300

    Menu lateral

--- src/Components/Accordion/index.tsx
+++ src/Components/Accordion/index.tsx
@@ -3,30 +3,50 @@ import * as AccordionRadix from '@radix-ui/react-accordion';
 import classNames from 'classnames';
 import { BsChevronDown } from 'react-icons/bs';
 
-const Accordion = ({ children, collapsible, defaultValue }: { children: React.ReactNode, collapsible?: boolean, defaultValue?: any }) => (
+type accordionProps = {
+    children: React.ReactNode,
+    collapsible?: boolean,
+    defaultValue?: any
+}
+
+const Accordion = ({ children, collapsible, defaultValue }: accordionProps) => (
     <AccordionRadix.Root className="AccordionRoot w-full" type="single" defaultValue={defaultValue} collapsible={collapsible}>
         {children}
     </AccordionRadix.Root>
 );
 
-const AccordionItem = ({ children, value }: { children: React.ReactNode, value: any }) => (
-    <AccordionRadix.Item className="AccordionItem" value={value}>
+type accordionItemProps = {
+    children: React.ReactNode,
+    value: any,
+}
+
+const AccordionItem = ({ children, value }: accordionItemProps) => (
+    <AccordionRadix.Item className="AccordionItem rounded-md" value={value}>
         {children}
     </AccordionRadix.Item>
 );
 
-AccordionItem.Trigger = React.forwardRef(({ children, className, ...props }: any, forwardedRef) => (
-    <AccordionRadix.Header className="AccordionHeader">
-        <AccordionRadix.Trigger
-            className={classNames('AccordionTrigger w-full', className)}
-            {...props}
-            ref={forwardedRef}
-        >
-            {children}
-            <BsChevronDown className="AccordionChevron" aria-hidden />
-        </AccordionRadix.Trigger>
-    </AccordionRadix.Header>
-));
+type accordionTrigger = {
+    children: React.ReactNode,
+    className: string,
+    collapsible: boolean,
+}
+
+AccordionItem.Trigger = React.forwardRef(({ children, className, collapsible, ...props }: accordionTrigger & any, forwardedRef) => {
+    return (
+        <AccordionRadix.Header className="AccordionHeader">
+            <AccordionRadix.Trigger
+                className={classNames('AccordionTrigger w-full pr-2 hover:bg-gray-100 dark:hover:bg-gray-700 ', className)}
+                {...props}
+                ref={forwardedRef}
+            >
+                {children}
+
+                {collapsible && <BsChevronDown className="AccordionChevron text-white" aria-hidden />}
+            </AccordionRadix.Trigger>
+        </AccordionRadix.Header>
+    )
+});
 
 AccordionItem.Content = React.forwardRef(({ children, className, ...props }: any, forwardedRef) => (
     <AccordionRadix.Content
--- src/Layouts/Dashboard/MenuItem/index.tsx
+++ src/Layouts/Dashboard/MenuItem/index.tsx
@@ -2,12 +2,12 @@ import { IconType } from 'react-icons';
 
 const MenuItem = ({ children, Icon, link }: { children: React.ReactNode, Icon: IconType, link: string }): React.ReactNode => {
     return (
-        <li>
-            <a href={link} onClick={e => !link && e.preventDefault()} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                {children}
-
+        <li className='w-full'>
+            <div onClick={() => console.log(link)} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group">
                 <Icon className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white " />
-            </a>
+
+                {children}
+            </div>
         </li>
     )
 }
